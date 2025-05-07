15055a6aac614778bb560d05a53a886f10c16ad0
Amul Op <amulsharmarr67@gmail.com>
2025-05-07 22:34:06 +0530

    Function how it works

---
+++ S1_Fundamentals/P3_Functions.js
@@ -0,0 +1,14 @@
+//How function works in JS?
+// As previous discussion when function is invoked Global execution context is created & pushed
+// to the call stack
+//It will first search for variable in local scope and if its not found it will check to the parent scope
+
+
+var num = 18;
+printNum();
+console.log(num);
+
+function printNum(){
+    // var num = 45;   //on commenting 18 18 will print
+    console.log(num);
+}
